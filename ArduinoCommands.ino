d75eea1a0e529d9725ede1c8e94c1d2e22d5b241
Danish Nadar <89051895+DanishNadar@users.noreply.github.com>
2025-03-06 13:22:52 -0600

    Update ArduinoCommands.ino
    New swap and iterativeIncrease functionalities included with test cases.

--- ArduinoCommands.ino
+++ ArduinoCommands.ino
@@ -8,28 +8,19 @@ void setup() {
  
 }
  
-// void loop() {
-//   // put your main code here, to run repeatedly:
-//   digitalWrite(MotorPin1,HIGH);
-//   digitalWrite(MotorPin2,LOW);
- 
-//   analogWrite(speedPin,(255/2));
-//   delay(1000);
- 
-//   digitalWrite(MotorPin1,LOW);
-//   digitalWrite(MotorPin2,HIGH);
- 
-//   analogWrite(speedPin,(255/2));
-//   delay(1000);
-// }
+void loop() {
+  notReallyAMacro();
+}
 
 // Choose a speed and # of times to reiterate
 // Possible speed range from 0 to 255
-void theFastAndTheFurious(int speed, int tiems = 1){
-
+void theFast(int speed, int times = 1, int increaseValue = 50){
     for(int i = 0; i < times; i++){
       analogWrite(speedPin, speed);
       delay(500);
+      if(increaseValue){
+        speed += increaseValue;
+      }
     }
   }
 }
@@ -37,17 +28,23 @@ void theFastAndTheFurious(int speed, int tiems = 1){
 // Choose a rotation and # of times to reiterate
 // Forward (1) -> High then Low
 // Bakcward (-1) -> Low -> High
-void theRollyPolly(int rollDirection, int times = 1, swap = false){
+void theFurious(int rollDirection, int times = 1, swap flip = false){
   for(int i = 0; i < times; i++){
     if(rollDirection == 1){
       digitalWrite(MotorPin1,HIGH);
       digitalWrite(MotorPin2,LOW);
-      delay(500)
+      delay(500);
     }
     if(rollDirection == -1){
       digitalWrite(MotorPin2,HIGH);
       digitalWrite(MotorPin1,LOW);
-      delay(500)
+      delay(500);
+    }
+    // Swaps rotation direction forward and backward
+    if(swap == true && rollDirection == 1){
+      rollDirection = -1; 
+    } else{
+      rollDirection = 1;
     }
   }
 }
@@ -55,13 +52,17 @@ void theRollyPolly(int rollDirection, int times = 1, swap = false){
 void notReallyAMacro(){
   // Include whatever function calls you want
   /*
-  theRollyPolly(1, 3);
-  theFastAndTheFurious(50,1);
-  theFastAndTheFurious(100,1);
-  theFastAndTheFurious(150,1);
-  theFastAndTheFurious(200,1);
-  theRollyPolly(-1,3);
-  theFastAndTheFurious(255,1);
+  theFast(speed = 50, times = 1);
+  theFast(speed = 50, times = 3);
+  theFurious(roll = 1, times = 4, swap = true);
+  theFast(speed = 50, times = 5, increaseValue = 50)
   */ 
+
+  // Below is a test 'macro'
+  theFast(speed = 50, times = 1);
+  theFast(speed = 50, times = 3);
+  theFurious(roll = 1, times = 1);
+  theFast(speed = 50, times = 5, increaseValue = 50)
+  theFurious(roll = -1, times = 4, swap = true)
 }
 
