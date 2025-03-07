e2139be12c5d4529a5412f213ee7595f57e375f1
Danish Nadar <89051895+DanishNadar@users.noreply.github.com>
2025-03-07 07:20:38 -0600

    Update ArduinoCommands.ino
    Fixed rollDirection logic and ensured comment coverage.

--- ArduinoCommands.ino
+++ ArduinoCommands.ino
@@ -16,28 +16,42 @@ void setup() {
 
 void driverControl(int speed, int increaseValue, int rollDirection, int times, bool swap){
   for(int i = 0; i < times; i++){
+    // setting speed (user & after increment)
     analogWrite(speedPin, speed);
-    digitalWrite(MotorPin1,HIGH);
-    digitalWrite(MotorPin2,LOW);
+    
+    // rollDirection functionality (if 1 -> forward) (if 2 -> backward)
+    if(rollDirection == 1){
+      digitalWrite(MotorPin1,HIGH);
+      digitalWrite(MotorPin2,LOW);
+    } else{
+      digitalWrite(MotorPin1,LOW);
+      digitalWrite(MotorPin2,HIGH);
+    }
 
+    // Increments speed value for each iteration if a value > 0 is provided
     if(increaseValue){
       speed += increaseValue;
     }
 
+    // Swaps direction from forward to backward (vice-versa) if 'swap' argument is given as true
     if(swap == true && rollDirection == 1){
       rollDirection = -1; 
     } else{
       rollDirection = 1;
     }
 
-    delay(500);
+    // Delay of 300 ms, creating a gap to see visible change in the next iteration 
+    delay(300);
   }
 }
 
+// Where the students will write their driverControl commands by calling methods with their specified parameters
 void notReallyAMacro(){
+  // Sample command (for testing)
   driverControl(50, 50, 1, 4, true);
 }
 
+// Similar to arduino's main function, which in our case will just call the function with a list of commands to execute. 
 void loop() {
   notReallyAMacro();
 }
