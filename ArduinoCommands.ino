99850ec9c500cd8b2e2852eb52114048c303388f
Danish Nadar <89051895+DanishNadar@users.noreply.github.com>
2025-03-07 02:06:49 -0600

    Update ArduinoCommands.ino
    Consolidated speed and rollDirection functions into one driverControl function.

--- ArduinoCommands.ino
+++ ArduinoCommands.ino
@@ -5,68 +5,37 @@ void setup() {
   pinMode(MotorPin1,OUTPUT);
   pinMode(MotorPin2,OUTPUT);
   pinMode(speedPin,OUTPUT);
- 
 }
- 
-// Choose a speed and # of times to reiterate
-// Possible speed range from 0 to 255
-void theFast(int speed = 20, int times = 1, int increaseValue = 50){
-    for(int i = 0; i < times; i++){
-      analogWrite(speedPin, speed);
-      delay(100);
-      if(increaseValue){
-        speed += increaseValue;
-      }
-    }
-  }
 
 // Choose a rotation and # of times to reiterate
 // Forward (1) -> High then Low
 // Bakcward (-1) -> Low -> High
-void theFurious(int rollDirection = 1, int times = 1, bool swap = false){
+
+// Choose a speed and # of times to reiterate
+// Possible speed range from 0 to 255
+
+void driverControl(int speed, int increaseValue, int rollDirection, int times, bool swap){
   for(int i = 0; i < times; i++){
-    if(rollDirection == 1){
-      digitalWrite(MotorPin1,HIGH);
-      digitalWrite(MotorPin2,LOW);
-      delay(100);
+    analogWrite(speedPin, speed);
+    digitalWrite(MotorPin1,HIGH);
+    digitalWrite(MotorPin2,LOW);
+
+    if(increaseValue){
+      speed += increaseValue;
     }
-    if(rollDirection == -1){
-      digitalWrite(MotorPin2,HIGH);
-      digitalWrite(MotorPin1,LOW);
-      delay(100);
-    }
-    // Swaps rotation direction forward and backward
+
     if(swap == true && rollDirection == 1){
       rollDirection = -1; 
     } else{
       rollDirection = 1;
     }
+
+    delay(500);
   }
 }
 
 void notReallyAMacro(){
-  // Include whatever function calls you want
-  /*
-  theFast(speed = 50, times = 1);
-  theFast(speed = 50, times = 3);
-  theFurious(roll = 1, times = 4, swap = true);
-  theFast(speed = 50, times = 5, increaseValue = 50)
-  */ 
-
-  // Below is a test 'macro'
-
-  //Initialized variables (default)
-  int speed = 10;
-  int times = 1;
-  int increaseValue = 25;
-  bool roll = false;
-  bool swap = false;
-
-  theFast(speed = 50, times = 1);
-  theFast(speed = 50, times = 3);
-  theFurious(roll = 1, times = 1);
-  theFast(speed = 50, times = 5, increaseValue = 50);
-  theFurious(roll = -1, times = 4, swap = true);
+  driverControl(50, 50, 1, 4, true);
 }
 
 void loop() {
