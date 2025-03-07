d46571e55be56c177763c26b39cbb754c52d4575
Danish Nadar <89051895+DanishNadar@users.noreply.github.com>
2025-03-06 19:45:22 -0600

    Update ArduinoCommands.ino

--- ArduinoCommands.ino
+++ ArduinoCommands.ino
@@ -8,37 +8,32 @@ void setup() {
  
 }
  
-void loop() {
-  notReallyAMacro();
-}
-
 // Choose a speed and # of times to reiterate
 // Possible speed range from 0 to 255
-void theFast(int speed, int times = 1, int increaseValue = 50){
+void theFast(int speed = 20, int times = 1, int increaseValue = 50){
     for(int i = 0; i < times; i++){
       analogWrite(speedPin, speed);
-      delay(500);
+      delay(100);
       if(increaseValue){
         speed += increaseValue;
       }
     }
   }
-}
 
 // Choose a rotation and # of times to reiterate
 // Forward (1) -> High then Low
 // Bakcward (-1) -> Low -> High
-void theFurious(int rollDirection, int times = 1, swap flip = false){
+void theFurious(int rollDirection = 1, int times = 1, bool swap = false){
   for(int i = 0; i < times; i++){
     if(rollDirection == 1){
       digitalWrite(MotorPin1,HIGH);
       digitalWrite(MotorPin2,LOW);
-      delay(500);
+      delay(100);
     }
     if(rollDirection == -1){
       digitalWrite(MotorPin2,HIGH);
       digitalWrite(MotorPin1,LOW);
-      delay(500);
+      delay(100);
     }
     // Swaps rotation direction forward and backward
     if(swap == true && rollDirection == 1){
@@ -59,10 +54,21 @@ void notReallyAMacro(){
   */ 
 
   // Below is a test 'macro'
+
+  //Initialized variables (default)
+  int speed = 10;
+  int times = 1;
+  int increaseValue = 25;
+  bool roll = false;
+  bool swap = false;
+
   theFast(speed = 50, times = 1);
   theFast(speed = 50, times = 3);
   theFurious(roll = 1, times = 1);
-  theFast(speed = 50, times = 5, increaseValue = 50)
-  theFurious(roll = -1, times = 4, swap = true)
+  theFast(speed = 50, times = 5, increaseValue = 50);
+  theFurious(roll = -1, times = 4, swap = true);
 }
 
+void loop() {
+  notReallyAMacro();
+}
